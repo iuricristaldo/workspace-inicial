b8ee9c7848339c4cce33de7a2adaa6e9f318ff08
iuricristaldo <iuricristaldo96@gmail.com>
2021-08-31 11:37:28 -0300

    Logout
    -Agregado funcionalidad al botón logout
    - Comentarios en checkLogin.js y login.js

--- js/checkLogin.js
+++ js/checkLogin.js
@@ -1,6 +1,16 @@
 //Obtengo el objeto usuario-loggeado de la session y valido que efectivamente haya un usuario loggeado
 
-if (sessionStorage.getItem("usuario-loggeado") === null) {
-    window.location.href = "login.html";
-    console.log(sessionStorage.getItem("usuario-loggeado"));
+if (localStorage.getItem("usuario-loggeado") === null) {
+    window.location.href = "login.html"; // redirecciona hacia login.html si el valor de la clave "usuario-loggeado" es null
+    console.log(localStorage.getItem("usuario-loggeado")); //console.log() usado para probar si el item se guardo en localStorage
+
 }
+
+document.getElementById("logout").addEventListener('click', function(e) {
+
+    localStorage.removeItem("usuario-loggeado");
+    window.location.href = "login.html";
+    /* - Elimina el item "usuario-loggeado" que aloja el nombre de usuario ingresado en login.
+         Se ejecuta al hacer click en el botón "Cerrar sesion" de id="logout"
+       - Luego redirecciona hacia el login */
+});
--- js/login.js
+++ js/login.js
@@ -1,20 +1,25 @@
 document.getElementById("loginbutton").addEventListener("click", function() {
-    // hice click en el boton
-    //Controlo nombre pass
+    // Se asigna una función al boton "Iniciar sesión" de id="loginbutton". La cual se ejecuta al hacer click sobre el mismo.
+
 
 
     let usuarioLoggeado = document.getElementById("usuario").value;
     let passLoggeada = document.getElementById("pass").value;
 
-    console.log(usuarioLoggeado)
-    console.log(passLoggeada)
+    console.log(usuarioLoggeado) //usado para comprobar que usuarioLoggeado haya recibido algún valor
+    console.log(passLoggeada) // usado para comprobar que passLoggeada haya recibido algún valor
 
     if (usuarioLoggeado === "" || passLoggeada === "") {
         alert("ingrese usuario y contraseña")
         return;
     }
+    /*Si el nombre o la contraseña son campos vacíos no permite seguir, se emite una alerta */
 
-    sessionStorage.setItem("usuario-loggeado", usuarioLoggeado);
-    window.location.replace("index.html");
+    localStorage.setItem("usuario-loggeado", usuarioLoggeado);
+    /*Cuando el usuario y la contraseña son ingresados se define el item "usuario-loggeado"
+      que aloja el valor ingresado en el campo de usuario */
+    window.location.href = "index.html";
+    // Redirecciona a "index.html"
+
 
 });
