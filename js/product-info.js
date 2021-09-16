9173e097d397d7edd3a9b5de43f1262406808dad
iuricristaldo <iuricristaldo96@gmail.com>
2021-09-16 09:46:47 -0300

    comentarios en product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -7,20 +7,21 @@ function starScore(stars) {
     let html = "";
     for (let i = 1; i <= number; i++) {
         html += '<i class="fa fa-star checked" aria-hidden="true"></i>';
-
+        /*Agrega tantas estrellas naranjas como lo indique el parámetro, 
+        en nuestro caso los parámetros que va a tomar la función son: comment.score y commentScore--> siempre están  entre 1 y 5 */
     }
     for (let j = number + 1; j <= 5; j++) {
         html += '<i class="fa fa-star " aria-hidden="true"></i>'
     }
     return html;
-
+    /*En caso de que la cantidad de estrellas sea menor a 5 completa con estrellas negras hasta llegar a 5, de esta forma se muestran siempre 5 estrellas, donde las naranjas corresponden el puntaje*/
 
 
 }
 
 
 function showImagesGallery(array) {
-
+    /*Agrega cada imagen a el html, el parámetro que toma para ejecutarse es el array: product.images , que contiene todas la imágenes del producto*/
     let htmlContentToAppend = "";
 
     for (let i = 0; i < array.length; i++) {
@@ -39,7 +40,7 @@ function showImagesGallery(array) {
 };
 
 function showComments() {
-
+    /*Agrega cada comentario del array al html*/
 
 
     let htmlContentToAppend = "";
@@ -79,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
             productNameHTML.innerHTML = product.name;
             productDescriptionHTML.innerHTML = product.description;
-            productPriceHTML.innerHTML = product.cost + " " + product.currency;
+            productPriceHTML.innerHTML = product.cost + " " + product.currency; /* Muestra el precio y la moneda dentro del tag asignado*/
             productSoldCountHTML.innerHTML = product.soldCount;
             productCategoryHTML.innerHTML = product.category;
 
@@ -90,6 +91,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(result) {
         if (result.status === "ok") {
             commentsArray = result.data;
+            /*Agrega los datos obtenidos al array commentsArray, luego ejecuta la función que agrega cada elemento al html*/
             showComments();
 
 
@@ -99,10 +101,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
 });
 
 document.getElementById("addComment").addEventListener("click", function() {
+    /*Función que permite agregar comentarios al clickear sobre el botón comentar*/
     let htmltoappend = "";
     let texto = document.getElementById("text-area").value;
     let date = new Date();
     var fecha = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    /*La función .getmonth() devuelve un valor entre 0 y 11, con 0 correspondiente a enero 1 a febrero y así sucesivamente, por lo que es necesario aumentarle 1 para ubicar a los meses entre 1 y 12*/
     let commentScore = document.getElementById('scoreSelected').value;
 
 
